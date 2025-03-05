c107a90edfbf1f21c4a4ecddea286b38d157b2e9
DB <2502523450@qq.com>
2025-03-05 15:54:14 +0800

    ✨ feat(breadcrumbs)：添加动态面包屑导航助手和集成

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,4 +1,12 @@
 class HomeController < ApplicationController
+  before_action :set_chat_breadcrumbs
   def index
+  end
+
+  private
+
+  def set_chat_breadcrumbs
+    set_default_breadcrumb
+    add_breadcrumb "Home", root_path
   end
 end
