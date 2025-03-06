c784652c98bd87ec43d5e9d398e660d2fc6bfc35
DB <2502523450@qq.com>
2025-03-06 19:55:12 +0800

    🔧 refactor(Assistant): 移除向量搜索相关代码
    - 删除 vectorsearch 相关的方法和回调
    - 简化 Assistant 模型定义
    - 移除未使用的向量搜索功能

--- app/models/assistant.rb
+++ app/models/assistant.rb
@@ -1,10 +1,6 @@
 # frozen_string_literal: true
 
 class Assistant < ActiveRecord::Base
-  vectorsearch
-
-  after_save :upsert_to_vectorsearch
-
   belongs_to :user
   has_many :messages, dependent: :destroy
   has_many :conversations, dependent: :destroy
